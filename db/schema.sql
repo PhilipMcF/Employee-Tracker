f39603e201c0bd718fddcab1e6361ff66a0d70c1
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-08-31 19:08:32 -0400

    created query functions to drop and create department, role and employee tables. working on inquirer to prompt for options and create corresponding functions.

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS department, role, employee;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name varchar(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salaray DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL REFERENCES role(id),
+);
+
+SELECT * FROM department, role, employee;
--- dist/index.js
+++ dist/index.js
@@ -1,53 +1,49 @@
 // import { QueryResult } from "pg";
 import { pool, connectToDB } from "./connection.js";
 import inquirer from 'inquirer';
-
 await connectToDB();
-
-//CREATE DB SCHEMA TABLE: -DROP TABLES AND CREATE department, role, employee TABLES:
+//CREATE DB SCHEMA TABLE
 async function dropTables() {
     try {
         await pool.query('DROP TABLE IF EXISTS employee, role, department;');
         console.log('TABLES DROPPED.');
-    } catch (err) {
+    }
+    catch (err) {
         console.error('ERROR WITH DROPPING TABLES: ' + err);
     }
 }
-
 async function createDepartmentTable() {
     try {
         await pool.query('CREATE TABLE department (id SERIAL PRIMARY KEY, name varchar(30) UNIQUE NOT NULL);');
         console.log('DEPARTMENT TABLE CREATED.');
-    } catch (err) {
+    }
+    catch (err) {
         console.error('ERROR WITH CREATING DEPARTMENT TABLE: ' + err);
     }
 }
-
 async function createRoleTable() {
     try {
         await pool.query('CREATE TABLE role (id SERIAL PRIMARY KEY, title VARCHAR(30) UNIQUE NOT NULL, salary DECIMAL NOT NULL, department_id INTEGER NOT NULL REFERENCES department(id));');
         console.log('ROLE TABLE CREATED.');
-    } catch (err) {
+    }
+    catch (err) {
         console.error('ERROR WITH CREATING ROLE TABLE: ' + err);
     }
 }
-
 async function createEmployeeTable() {
     try {
         await pool.query('CREATE TABLE employee (id SERIAL PRIMARY KEY, first_name VARCHAR(30) NOT NULL, last_name VARCHAR(30) NOT NULL, role_id INTEGER REFERENCES role(id), manager_id INTEGER REFERENCES employee(id));');
         console.log('EMPLOYEE TABLE CREATED.');
-    } catch (err) {
+    }
+    catch (err) {
         console.error('ERROR WITH CREATING EMPLOYEE TABLE: ' + err);
     }
 }
-
 async function createTables() {
     await createDepartmentTable();
     await createRoleTable();
     await createEmployeeTable();
 }
-
-
 //PROMPT BASE FUNCTIONALITY
 // - VIEW ALL EMPLOYEES
 // - ADD EMPLOYEE
@@ -57,63 +53,36 @@ async function createTables() {
 // - VIEW ALL DEPARTMENTS
 // - ADD DEPARTMENT
 // - QUIT 
-
-function prompt(){
-    let exit: boolean = false;
+function prompt() {
+    let exit = false;
     inquirer
         .prompt([
-            {
-                type: 'list',
-                name: 'action',
-                message: 'What would you like to do?',
-                choices: [
-                    'View All Employees',
-                    'Add Employee',
-                    'Update Employee Role',
-                    'View All Roles',
-                    'Add Role',
-                    'View All Departments',
-                    'Add Department',
-                    'Quit',
-                ],
-            },
-        ])
-        .then((answers) => {
-            if (answers.action === 'View All Employees'){
-                console.log('VIEW ALL EMPLOYEES');
-            }
-            else if (answers.action === 'Add Employee'){
-
-            }
-            else if (answers.action === 'Update Employee Role'){
-
-            }
-            else if (answers.action === 'View all Roles'){
-
-            }
-            else if (answers.action === 'Add Role'){
-
-            }
-            else if (answers.action === 'View All Departments'){
-
-            }
-            else if (answers.action === 'Add Department'){
-
-            }
-            else {
-                exit = true;
-            }
-            if (!exit){
-                prompt();
-            }
-        });
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'Quit',
+            ],
+        },
+    ])
+        .then((answers));
+    {
+        if (answers.action === 'View All Employees') {
+            console.log('VIEW ALL EMPLOYEES');
+        }
+    }
 }
-
-
-async function init(){
+async function init() {
     await dropTables();
     await createTables();
     prompt();
 }
-
 init();
